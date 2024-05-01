470f6cd0bfc3563e9f900e63a06bd3ea19dbb9c9
wyhwong <wyhwong@link.cuhk.edu.hk>
2024-05-01 14:47:37 +0800

    Relocation

---
+++ climsim-2024/src/schemas/visualization.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Padding:
+    """Padding of the plot."""
+
+    tpad: float = 2.5
+    lpad: float = 0.1
+    bpad: float = 0.12
+
+
+@dataclass
+class Labels:
+    """Labels of the plot."""
+
+    title: str = ""
+    xlabel: str = ""
+    ylabel: str = ""
+    legend_title: str = ""
+
+
+@dataclass
+class Line:
+    """Line of the plot."""
+
+    left_bottom: tuple[float, float]
+    right_top: tuple[float, float]
+    color: str = "blue"
+    linestyle: str = "--"
+    label: str = ""
