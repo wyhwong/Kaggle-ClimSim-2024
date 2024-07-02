039ae1edeb88b28c8efa3aa46f5fb3f1a6c8fa72
wyhwong <wyhwong@link.cuhk.edu.hk>
2024-07-03 00:06:52 +0800

    R2 score fix

--- climsim-2024/src/pytorch/loss/r2.py
+++ climsim-2024/src/pytorch/loss/r2.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 import src.logger
@@ -21,10 +22,10 @@ def r2_score_multivariate(y_hat: torch.Tensor, y: torch.Tensor) -> torch.tensor:
     mean_targets = torch.mean(y, dim=0)
 
     ss_total = torch.sum(torch.square(y - mean_targets), dim=0)
-    ss_total[ss_total == 0] = 1.0
     ss_residual = torch.sum(torch.square(y - y_hat), dim=0)
 
     uni_r2 = 1.0 - ss_residual / ss_total
+    uni_r2[uni_r2 == -np.inf] = 0.0
 
     r2 = torch.nanmean(uni_r2[torch.isfinite(uni_r2)])
     local_logger.debug("R2 computed: %4f, n_dim: %s", r2, uni_r2.shape)
