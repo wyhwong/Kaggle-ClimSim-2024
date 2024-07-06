89c31a23a1a90a5c7a82002efd57d0f49a3ed2f8
wyhwong <wyhwong@link.cuhk.edu.hk>
2024-07-06 12:16:30 +0800

    Clip R2

--- climsim-2024/src/pytorch/loss/r2.py
+++ climsim-2024/src/pytorch/loss/r2.py
@@ -25,7 +25,7 @@ def r2_score_multivariate(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     ss_residual = torch.sum(torch.square(y - y_hat), dim=0)
 
     uni_r2 = 1.0 - ss_residual / ss_total
-    uni_r2[uni_r2 == -np.inf] = 0.0
+    uni_r2 = uni_r2.clip(0.0, 1.0)
 
     r2 = torch.nanmean(uni_r2[torch.isfinite(uni_r2)])
     local_logger.debug("R2 computed: %4f, n_dim: %s", r2, uni_r2.shape)
